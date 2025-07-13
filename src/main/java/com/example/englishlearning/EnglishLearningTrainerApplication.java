73fae667de57c010695f0ba14b346716c4d69ff2
artificialLeo <horkovenko.k@gmail.com>
2025-07-13 13:43:19 +0300

    remove unnecessary blank lines in main application class

--- src/main/java/com/example/englishlearning/EnglishLearningTrainerApplication.java
+++ src/main/java/com/example/englishlearning/EnglishLearningTrainerApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EnglishLearningTrainerApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(EnglishLearningTrainerApplication.class, args);
-
 	}
 
 }
