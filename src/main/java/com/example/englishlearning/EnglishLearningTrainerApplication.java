bdd86b1d04a70359834ca0573a988c791e5433a7
artificialLeo <horkovenko.k@gmail.com>
2025-07-13 13:42:28 +0300

    resolved

---
+++ src/main/java/com/example/englishlearning/EnglishLearningTrainerApplication.java
@@ -0,0 +1,15 @@
+package com.example.englishlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishLearningTrainerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EnglishLearningTrainerApplication.class, args);
+
+	}
+
+}
